56ab52ae0eef4f0fe369d2818d8a1e4077578356
TrongPoly <trongnguyen32293@gmail.com>
2023-05-25 17:49:19 +0700

    update giao dien
    cập nhật lại link css, js, img. Thêm controller chuyển trang

--- src/main/java/com/fpoly/controller/IndexController.java
+++ src/main/java/com/fpoly/controller/IndexController.java
@@ -1,9 +1,17 @@
 package com.fpoly.controller;
 
+import java.lang.ProcessBuilder.Redirect;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fpoly.model.TaiKhoan;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class IndexController {
@@ -12,14 +20,31 @@ public class IndexController {
 		return "views/user/index";
 	}
 
+	@RequestMapping("/product/laptop")
+	public String latop() {
+		return "views/user/MayTinh";
+	}
+
+	@RequestMapping("/product/details")
+	public String details() {
+		return "views/user/ChiTietSP";
+	}
+
+	@RequestMapping("/regresion")
+	public String Regres() {
+		return "views/user/DangKy";
+	}
+
 	@GetMapping("/login")
-	public String formLogin() {
+	public String formLogin(@ModelAttribute("taikhoan") TaiKhoan taiKhoan) {
 		return "views/user/login";
 	}
 
 	@PostMapping("/login")
-	public String login() {
-		// Xử lý
+	public String login(@Valid @ModelAttribute("taikhoan") TaiKhoan taiKhoan, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "views/user/login";
+		}
 		return "views/user/index";
 	}
 }
